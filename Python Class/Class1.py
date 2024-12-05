ba40d0849d04d7fb6b27471438c61a01e026a227
Manish <m4manishp4prasad@gmail.com>
2024-12-05 23:42:39 +0530

    Added Class

---
+++ Python Class/Class1.py
@@ -0,0 +1,34 @@
+# [CLASS] : 03 Dec, 24
+
+
+# target = int(input("Enter target"))
+# list = [1,2,3,4,5,6,7,8,9]
+# for i in list:
+#     for j in list:
+#         if i+j==target:
+#             print(i,",",j)
+
+
+
+
+# list = ["Ram","adarsh","Raju"]
+# # count = 0
+# for i in list:
+#     count = 0
+#     for j in i:
+#         if j == "a" or j == "e" or j == "i" or j == "o" or j == "u":
+#             count += 1
+#             if count == 2:
+#                 print(i)
+#                 # count =0
+
+
+
+
+# list = "Hi there, this is a a demo text to to check the the deletion"
+# st = list.split(" ")
+# alt = []
+# for data in st:
+#     if data not in alt:
+#         alt.append(data)
+# print(alt)
--- Python Lab/Lab 2.py
+++ Python Lab/Lab 2.py
@@ -112,39 +112,3 @@ print("Perimeter of rectangle with lenght ", {lenght}, "and breadth ", {breadth}
 
 # Homework: Write a program to take input (numbers) separeted by coma and then find the target sum of two numbers without using SPLIT function.
 
-
-
-# [CLASS] : 03 Dec, 24
-
-
-# target = int(input("Enter target"))
-# list = [1,2,3,4,5,6,7,8,9]
-# for i in list:
-#     for j in list:
-#         if i+j==target:
-#             print(i,",",j)
-
-
-
-
-# list = ["Ram","adarsh","Raju"]
-# # count = 0
-# for i in list:
-#     count = 0
-#     for j in i:
-#         if j == "a" or j == "e" or j == "i" or j == "o" or j == "u":
-#             count += 1
-#             if count == 2:
-#                 print(i)
-#                 # count =0
-
-
-
-
-# list = "Hi there, this is a a demo text to to check the the deletion"
-# st = list.split(" ")
-# alt = []
-# for data in st:
-#     if data not in alt:
-#         alt.append(data)
-# print(alt)
