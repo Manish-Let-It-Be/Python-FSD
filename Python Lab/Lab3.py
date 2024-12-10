27707009874f42caff5f37b664f9a1ea4aeddf16
Manish <m4manishp4prasad@gmail.com>
2024-12-10 20:01:47 +0530

    Practical3

--- Python Lab/Lab3.py
+++ Python Lab/Lab3.py
@@ -1,2 +1,20 @@
 # LAB : 3
 # 10 Dec, 24
+"""
+Performed all the Practicals of Set 1
+"""
+
+# PRACTICAL : 
+# Set 1 => Practical 3.
+# A program that generates a random password of a specified lenght.
+
+import random
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for i in range(length))
+    return password
+
+length = int(input("Enter the desired password length: "))
+print("Generated password:", generate_password(length))
