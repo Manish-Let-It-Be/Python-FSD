e1d271cd5894ee9cc414edda8a1a15f140b5f101
Manish <m4manishp4prasad@gmail.com>
2024-12-28 22:31:51 +0530

    Query

--- PythonWithSQL/query.sql
+++ PythonWithSQL/query.sql
@@ -1,7 +1,8 @@
--- Active: 1735060077112@@127.0.0.1@3306@school_ms
+-- Active: 1735060077112@@127.0.0.1@3306@demo_user
 use school_ms;
 
 select * from students;
 
 use demo_user;
 select * from user_data;
+select * from user_data where id=4;
